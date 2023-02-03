724cc18c8e62c007ade53aa03dbd414125ce0385
svonme <svon.me@gmail.com>
2023-02-03 23:39:15 +0800

    doc: 维护 types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
  * @author svon.me@gmail.com
  */
 
+export { useValidate } from "./utils";
 export { config } from "./config";
 export { model } from "./logic/form";
 export { default as Form } from "./components/form";
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,8 +3,10 @@
  * @author svon.me@gmail.com
  */
 
+import { PropType } from "vue";
 import { FormOptionValue } from "./form";
 import type { Component } from "vue";
+import type { FormInstance, ModalFuncProps } from "ant-design-vue";
 
 export * from "./form";
 
@@ -12,8 +14,78 @@ export declare interface State {
   [key: string]: any;
 }
 
+export declare const config: (name: string, component: Component) => void;
+
 export declare const model: <T = State>(value: FormOptionValue, config?: string | ModalFuncProps) => Promise<T | undefined>;
 
-export declare const config: (name: string, component: Component) => void
+export declare const useValidate: () => {
+    formRef: import("vue").Ref<FormInstance>;
+    validate: <T>(next?: () => T | Promise<T>) => Promise<boolean | T>;
+};
 
-export declare const Form: typeof import("../src/components/form");
+interface FormState {
+    [key: string]: any;
+}
+declare enum Layout {
+    horizontal = "horizontal",
+    vertical = "vertical",
+    inline = "inline"
+}
+
+export declare const Form: import("vue").DefineComponent<{
+    value: {
+        type: PropType<FormState>;
+        default(): {};
+    };
+    items: {
+        required: true;
+        type: PropType<FormOptionValue>;
+    };
+    option: {
+        type: PropType<ModalFuncProps>;
+        default(): {};
+    };
+    layout: {
+        type: PropType<string>;
+        default(): Layout;
+    };
+    class: {
+        type: StringConstructor;
+        default: string;
+    };
+    buttons: {
+        type: PropType<boolean | Component<any, any, any, import("vue").ComputedOptions, import("vue").MethodOptions>>;
+        default: () => boolean;
+    };
+}, () => JSX.Element, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    value: {
+        type: PropType<FormState>;
+        default(): {};
+    };
+    items: {
+        required: true;
+        type: PropType<FormOptionValue>;
+    };
+    option: {
+        type: PropType<ModalFuncProps>;
+        default(): {};
+    };
+    layout: {
+        type: PropType<string>;
+        default(): Layout;
+    };
+    class: {
+        type: StringConstructor;
+        default: string;
+    };
+    buttons: {
+        type: PropType<boolean | Component<any, any, any, import("vue").ComputedOptions, import("vue").MethodOptions>>;
+        default: () => boolean;
+    };
+}>>, {
+    buttons: boolean;
+    value: {};
+    option: {};
+    layout: string;
+    class: string;
+}>;
